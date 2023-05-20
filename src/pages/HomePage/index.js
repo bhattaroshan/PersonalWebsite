f12eaf2e3a791721bc5190f5c444d3e195508ef9
Roshan Bhatta <bhattaroshan@gmail.com>
2023-05-20 10:41:01 +0545

    added homepage context

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -10,22 +10,31 @@ function HomePage() {
     useEffect(()=>{
         const sketch = new p5((sketch)=>{
             let nodes = [];
-            const numNodes = 20;
+            const numNodes = 10;
+            let categories = [
+                "Food","Travel","Books","Drive","Passion","Educator",
+                "Programmer","Engineer","Music","Curious"
+            ];
 
             class Node {
-                constructor(x, y) {
+                constructor(x, y,i) {
                   this.x = x;
                   this.y = y;
-                  this.radius = 25;
+                  this.radius = 70;
                   this.speedX = p.random(-2, 2);
                   this.speedY = p.random(-2, 2);
                   this.color = p.color(p.random(0,50), p.random(50,100), p.random(100,200));
+                  this.text = categories[i];
                 }
 
                 display() {
                     p.fill(this.color);
                     p.noStroke();
                     p.ellipse(this.x, this.y, this.radius);
+                    p.fill(200,200,200);
+                    p.textSize(12);
+                    p.textAlign(p.CENTER,p.CENTER);
+                    p.text(this.text,this.x,this.y);
                   }
           
                 update() {
@@ -46,35 +55,53 @@ function HomePage() {
             p.setup = () =>{
                 canvas = p.createCanvas(p.windowWidth,400).parent(sketchRef.current);
                 for (let i = 0; i < numNodes; i++) {
-                    const x = p.random(p.width);
-                    const y = p.random(p.height);
-                    nodes.push(new Node(x, y));
+                    let x = p.random(p.width);
+                    let y = p.random(p.height);
+                    let newPos = true;
+
+                    while(newPos){
+                        newPos = false;
+                        for(let i=0;i<nodes.length;i++){
+                            const node = nodes[i];
+                            const d = p.dist(node.x,node.y,x,y);
+                            if(d<=node.radius*3){
+                                newPos = true;
+                                break;
+                            }
+                        }
+                        x = p.random(p.width);
+                        y = p.random(p.height);
+                    }
+                   
+
+
+                    nodes.push(new Node(x, y,i));
                   }
             }
 
             p.draw = () =>{
-                p.background(25,25,25);
-                //p.ellipse(p.mouseX,p.mouseY,50,50);
+                p.background(10,10,10);
 
                   // Draw edges between nodes
-                  for (let i = 0; i < nodes.length; i++) {
+                for (let i = 0; i < nodes.length; i++) {
                     const nodeA = nodes[i];
                     for (let j = i + 1; j < nodes.length; j++) {
-                      const nodeB = nodes[j];
-                      const d = p.dist(nodeA.x, nodeA.y, nodeB.x, nodeB.y);
-                      if(d<nodeA.radius){
+                        const nodeB = nodes[j];
+                        const d = p.dist(nodeA.x, nodeA.y, nodeB.x, nodeB.y);
+                        if(d<nodeA.radius){
                         nodeA.speedX *= -1;
                         nodeA.speedY *= -1;
                         nodeB.speedX *= -1;
                         nodeB.speedY *= -1;
-                      }
-
-                      if (d < 100) {
-                        p.stroke(128,128,128);
+                        }
+                        const forceDist = 300;
+                        if (d < forceDist) {
+                        p.stroke(50,50,100);
+                        p.strokeWeight(((forceDist-d)*4)/forceDist);
                         p.line(nodeA.x, nodeA.y, nodeB.x, nodeB.y);
-                      }
+                        }
                     }
-                  }
+                }
 
                 for (let i = 0; i < nodes.length; i++) {
                     const node = nodes[i];
