e462cd467f380743178d479ad378f7efd9a60579
Roshan Bhatta <bhattaroshan@gmail.com>
2023-06-19 01:11:11 +0545

    added academic page

--- src/pages/AcademicsPage/index.js
+++ src/pages/AcademicsPage/index.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import {Box} from '@mui/material';
 import Timeline from '@mui/lab/Timeline';
 import TimelineItem from '@mui/lab/TimelineItem';
 import TimelineSeparator from '@mui/lab/TimelineSeparator';
@@ -12,61 +13,99 @@ import HotelIcon from '@mui/icons-material/Hotel';
 import RepeatIcon from '@mui/icons-material/Repeat';
 import Typography from '@mui/material/Typography';
 import {gsap} from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import ScrollTrigger from 'gsap/ScrollTrigger';
 import { duration } from '@mui/material';
 import { useEffect } from 'react';
 
 gsap.registerPlugin(ScrollTrigger);
 
+const academics = [
+    {
+        name: "Paragon Public School",
+        location: "Battisputali, Kathmanud",
+        year: 1998,
+    },
+    {
+        name: "Little Flower School",
+        location: "Narayanghat, Chitwan",
+        year: 2004,
+    },
+    {
+        name:"NIST",
+        location:"Lainchaur, Kathmandu",
+        year: 2006,
+    },
+    {
+        name:"Kathmandu Engineering College",
+        location:"Kalimati, Kathmandu",
+        year: 2008,
+    }
+];
+
+
 export default function AcademicsPage() {
 
     useEffect(()=>{
-        gsap.fromTo(".test_para",{
-                x: -window.innerWidth,
-                opacity: 0
-            },{
-                x: 0,
-                opacity: 1,
-                duration: 0.5,
-            }
-        )
-        gsap.fromTo(".test_para2",{
-            x: -window.innerWidth,
+        gsap.fromTo(".line",{
+            scale:0,
+            opacity:0,
         },{
-            x:0,
+            scale:1,
+            opacity:1,
             duration: 1,
-            scrollTrigger:{
-                trigger: '.test_para',
-                start: 'bottom center',
-                end: 'bottom end',
-                scrub: true,
-                ease: 'power2.out',
-            }
-        })
-        
-        gsap.fromTo(".test_para3",{
-            x: window.innerWidth,
-        },{
-            x:0,
-            duration: 1,
-            scrollTrigger:{
-                trigger: '.test_para2',
-                start: 'bottom center',
-                end: 'bottom end',
-                scrub: true,
-                ease: 'power2.out',
-            }
+            ease: 'power2'
         })
 
+        gsap.fromTo(".box",{
+            x: window.innerWidth,
+            scale: 0,
+            opacity: 0,
+            ease: 'power3'
+        },{
+            x:0,
+            scale: 1,
+            opacity:1,
+            delay: 0.4,
+            duration: 0.5
+        })
+
+        gsap.fromTo(".date",{
+            x: -window.innerWidth,
+            scale: 0,
+            opacity: 0,
+            ease: 'power3'
+        },{
+            x:0,
+            scale: 1,
+            opacity:1,
+            delay: 0.4,
+            duration: 0.5
+        })
     },[])
 
     return (
-        <div style={{display:'flex', flexDirection:'column', alignItems:'center'}}>
-            <div className="test_para" style={{width:'300px', height:'300px',background:'red'}}/>
-            <div className="test_para2" style={{width:'300px', height:'300px',background:'green'}}/>
-            <div className="test_para3" style={{width:'300px', height:'300px',background:'blue'}}/>
-            <div className="test_para4" style={{width:'300px', height:'300px',background:'yellow'}}/>
-            <div className="test_para5" style={{width:'300px', height:'300px',background:'pink'}}/>
-        </div>
+        <Box  sx={{display:'flex', flexDirection:'column', alignItems:'center', marginBottom:'100px'}}>
+            <Box sx={{minHeight:'20vh', minWidth:'100%', display:'flex', 
+                    justifyContent:'center', alignItems:'center'}}>
+            <Typography sx={{fontSize:'50px', mb:'40px', mt:'40px', fontWeight:'700'}}>Academics</Typography>
+            </Box>
+            <div style={{display:'flex', flexDirection:'column', alignItems:'center',margin:'20px', gap:'20px'}}>
+                {
+                    academics.map((v,i)=>{
+                        return  <div style={{display:'flex',gap:'40px'}} key={i}>
+                        <Typography className="date" style={{alignSelf:'center', textAlign:'right', fontSize:'20px', width:'10vw'}}>{v.year}</Typography>
+                        <div className="line" style={{width:'0.1px', height:'20vh', border:'2px solid #c0c0c0'}}/>
+                        <div className="box" style={{width:'60vw', heigth:'10vh', 
+                                                    border:'1px solid #0c0c0c', borderRadius: '10px',
+                                                    background:'#505050'}}>
+                            <Typography style={{margin:'10px', color:'white',fontSize:'20px', fontWeight:700}}>{v.name}</Typography>
+                            <Typography style={{margin:'10px', color:'white',fontSize:'14px', fontWeight:100}}>{v.location}</Typography>
+                        </div>
+                    </div>
+                    })
+                } 
+            </div>
+
+        </Box>
     );
 }
