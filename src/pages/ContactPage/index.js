ffa53c38b52a3cfd4d5277350db0be68725c64ab
roshanbhatta <bhattaroshan@gmail.com>
2023-12-06 15:51:56 +0545

    added contact me

--- src/pages/ContactPage/index.js
+++ src/pages/ContactPage/index.js
@@ -1,4 +1,4 @@
-import { Grid,Card,CardMedia,Box, Typography } from '@mui/material';
+import { Grid,Card,CardMedia,Box, Typography,TextField, Button } from '@mui/material';
 import React from 'react';
 import AboutImage from '../../assets/images/about-photo.jpeg';
 import FacebookIcon from '@mui/icons-material/Facebook';
@@ -10,10 +10,14 @@ function ContactPage() {
             <Grid item xs={8} md={5} lg={4}>
                 <Card>
                     <CardMedia 
-                        sx={{objectFit:'cover'}}
+                        sx={{objectFit:'cover', my:5}}
                         component="img"
                         image= {AboutImage}
                     /> 
+                <Box display='flex' justifyContent='center' gap={2} my={5}>
+                    <FacebookIcon fontSize='large'/>
+                    <YouTubeIcon fontSize='large'/>
+                </Box>
                 </Card>
             </Grid>
 
@@ -22,27 +26,36 @@ function ContactPage() {
                                 fontSize:{
                                     xs: '40px',
                                     sm: '40px',
-                                    md: '60px',
-                                    lg: '60px'
+                                    md: '40px',
+                                    lg: '40px'
 
                                 }, 
-                                fontWeight:'900'}}>ROSHAN</Typography>
+                                fontWeight:'600'}}>ROSHAN</Typography>
                 <Typography sx={{
                                 fontSize:{
                                     xs: '40px',
                                     sm:'40px',
-                                    md:'60px',
-                                    lg: '60px'
+                                    md:'40px',
+                                    lg: '40px'
                                 },
-                                fontWeight:'900', color:'#ee5500'}}>BHATTA</Typography>
-                <Typography display='inline' sx={{fontSize:'20px', fontWeight:'200', my:4, mr:{md:6}, textAlign:'justify'}}>
+                                fontWeight:'600', color:'#ee5500'}}>BHATTA</Typography>
+                <Typography display='inline' sx={{fontSize:'18px', fontWeight:'200', my:1, mr:{md:6}, textAlign:'justify'}}>
                     Passionate engineering manager with a strong backround in cloud computing, embedded programming and STEAM education. Keen enthusiast of DSP, applied mathematics, sound engineering and DSA.
                 </Typography>
-                <Box display='flex' justifyContent='center' gap={2} my={5}>
-                    <FacebookIcon fontSize='large'/>
-                    <YouTubeIcon fontSize='large'/>
-                </Box>
-
+                <Typography sx={{py:'10px', fontSize:'32px', fontWeight:700, justifyContent:'center', alignItems:'center'}}>Contact Me</Typography>
+                <TextField id="outlined-basic" label="Name" variant="outlined" sx={{py:'10px'}}/>
+                <TextField id="outlined-basic" label="Email" variant="outlined" sx={{py:'10px'}}/>
+                <TextField
+                            sx={{py:'10px'}}
+                            rows={4}
+                            id="standard-multiline-flexible"
+                            label="Message"
+                            multiline
+                            maxRows={4}
+                            // value={value}
+                            // onChange={handleChange}
+                />
+                <Button variant='contained' sx={{height:'50px'}}>Send Message</Button>
             </Grid>
             {/* <Grid item xs={12}>
                 <Typography>Test</Typography>
