87de424f8ac38572036a9b16bd72211d41cc92b7
roshanbhatta <bhattaroshan@gmail.com>
2024-01-01 19:45:36 +0545

    added scrolling country boundary

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -3,7 +3,8 @@ import {useState} from 'react';
 import {Button,Box} from '@mui/material';
 import L from 'leaflet';
 import travelPlaces from '../../contants/places';
-import {useMapEvents} from "react-leaflet";
+import {Polyline, useMapEvents} from "react-leaflet";
+import { geoNepal,polygon } from '../../config';
 
 import {
     MapContainer,
@@ -12,15 +13,23 @@ import {
     Marker,
     Popup,
     Circle,
+    Polygon,
   } from 'react-leaflet';
 import { Flag } from '@mui/icons-material';
 
+
 function Map() {
+ 
+  const [globalLevel,setGlobalLevel] = useState(3);
+
     const handleZoomEnd = (event) => {
         const zoomLevel = event.target.getZoom();
         console.log('Zoom level:', zoomLevel);
+        setGlobalLevel(zoomLevel);
         // Perform actions based on the zoom level change
         // ...
+        //until zoom level 5 from 0 map out only counties
+
       };
     
       function ZoomEvent() {
@@ -30,6 +39,7 @@ function Map() {
     
         return null;
       }
+      const fillBlueOptions = { fillColor: 'blue' }
 
     return (
         <MapContainer center={travelPlaces[0].latlong} zoom={3} scrollWheelZoom={true}> 
@@ -37,14 +47,9 @@ function Map() {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         <ZoomEvent />
-        {
+        {/* {
             travelPlaces.map((v,i)=>{
-                // return <Marker key={i} position={v} style={{color:'red'}}>
-                //         <Popup>
-                //             <Button>Test</Button>
-                //         </Popup>
-                //         <Circle center={v} pathOptions={{ fillColor: 'green',color: 'green' }} radius={1000} />
-                //     </Marker>
+                
                 return <Circle key={i} center={v.latlong} pathOptions={{ fillColor: 'red',color: 'red' }} radius={8000}>
                         <Popup>
                             {v.place}
@@ -53,7 +58,23 @@ function Map() {
 
             })
         }
-      
+       */}
+        {/* <Polygon pathOptions={fillBlueOptions} positions={geoNepal} /> */}
+        {
+          globalLevel<=5?
+            <Polygon pathOptions={{fillColor:'red',color:'red'}} positions={geoNepal}  />
+          :
+          travelPlaces.map((v,i)=>{
+                
+            return <Circle key={i} center={v.latlong} pathOptions={{ fillColor: 'red',color: 'red' }} radius={8000}>
+                    <Popup>
+                        {v.place}
+                    </Popup>
+                    </Circle>
+
+        }) 
+
+        }
       </MapContainer>
 
       );
